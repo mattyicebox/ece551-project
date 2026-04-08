ffdc82ee31bf53d129c1514cebe652f7421d589c
ColeMov <cole.movsessian@gmail.com>
2026-04-08 11:43:50 -0500

    Fix piezo_drv_test wiring

--- piezo/piezo_drv_test.sv
+++ piezo/piezo_drv_test.sv
@@ -1,7 +1,7 @@
-module piezo_drv_test(clk, RST_n, Batt_low, fanfare, piezo, piezo_n);
+module piezo_drv_test(clk, RST_n, batt_low, fanfare, piezo, piezo_n);
     input logic clk;
     input logic RST_n;
-    input logic Batt_low;
+    input logic batt_low;
     input logic fanfare;
     output logic piezo;
     output logic piezo_n;
@@ -9,14 +9,15 @@ module piezo_drv_test(clk, RST_n, Batt_low, fanfare, piezo, piezo_n);
     logic rst_n;
 
     reset_synch iRSTn_sync (
+        .RST_n(RST_n),
         .clk(clk),
-        .rst_n(RST_n)
+        .rst_n(rst_n)
     );
 
     piezo_drv #(.FAST_SIM(1'b0)) iDUT (
         .clk(clk),
         .rst_n(rst_n),
-        .batt_low(Batt_low),
+        .batt_low(batt_low),
         .fanfare(fanfare),
         .piezo(piezo),
         .piezo_n(piezo_n)
